bf106a757a51a77689b2918e7045c0ea5633c1f1
blessingarizona <blessingarinze9@gmail.com>
2024-04-04 14:54:58 +0100

    0x01-react_intro

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -12,9 +12,16 @@ function App() {
       </div>
       <div className='App-body'>
         <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email"></input>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password"></input>
+          <button>OK</button>
+        </form>
       </div>
       <div className='App-footer'>
-        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+        <p>Copyright {getFullYear()} - {getFooterCopy()} - holberton School</p>
       </div>
     </div>
 
