7c168b160acdb758849a51dbd798150a2b4ca461
blessingarizona <blessingarinze9@gmail.com>
2024-12-03 23:16:40 +0100

    react redux

---
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -0,0 +1,45 @@
+import * as actionTypes from '../actions/uiActionTypes';
+import * as actions from '../actions/uiActionCreators';
+import { selectCourse } from '../actions/courseActionCreators';
+import uiReducer from './uiReducer';
+
+describe('uiReducer', () => {
+
+    it('returns the same state as initialState when no action is passed', () => {
+        const testInitialState = {
+            isNotificationDrawerVisible: true,
+            isUserLoggedIn: false,
+            user: {}
+        };
+    
+        expect(uiReducer(testInitialState, {type: actionTypes.TEST})).toEqual(testInitialState);  
+    });
+
+    it('returns the same state as initialState when the action \
+        SELECT_COURSE is passed', () => {
+            
+        const testInitialState = { 
+            isNotificationDrawerVisible: true,
+            isUserLoggedIn: false,
+            user: {}
+        };
+
+        expect(uiReducer(testInitialState, selectCourse)).toEqual(testInitialState);
+    });
+
+    it('changes isNotificationDrawerVisible when DISPLAY(HIDE)_NOTIFICATION_DRAWER is passed', () => {
+        const testInitialState = {
+            isNotificationDrawerVisible: false,
+            isUserLoggedIn: false,
+            user: {}
+        };
+
+        let reducedState = uiReducer(testInitialState, actions.displayNotificationDrawer()).toJS();
+        console.log(reducedState)
+        expect(reducedState.isNotificationDrawerVisible).toEqual(true);
+
+        reducedState = uiReducer(reducedState, actions.hideNotificationDrawer()).toJS();
+        expect(reducedState.isNotificationDrawerVisible).toEqual(false);
+
+    })
+});
